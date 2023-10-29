8e6b78c38b809656ebfe42c8a6a79a49f88add95
archfi <mughni@mhs.usk.ac.id>
2023-10-29 10:25:28 +0700

    Add new method getNamaLevel

--- Pemain.java
+++ Pemain.java
@@ -2,7 +2,7 @@
 class Pemain {
     private String nama;
     private int nilai;
-
+    
     public Pemain(String nama) {
         this.nama = nama;
         this.nilai = 0;
@@ -22,5 +22,20 @@ class Pemain {
 
     public void setNilai(int nilaiBaru) {
         nilai = nilaiBaru;
+    }
+    
+    public String getNamaLevel(int level) {
+        switch (level) {
+            case 1:
+                return "Easy";
+            case 2:
+                return "Intermediate";
+            case 3:
+                return "Hard";
+            case 4:
+                return "Impossible";
+            default:
+                return "Unknown";
+        }
     }
 }
