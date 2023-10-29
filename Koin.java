98a1c40ba5408a85674be4c194830bf2a9f6289a
archfi <mughni@mhs.usk.ac.id>
2023-10-29 11:29:54 +0700

    Add javadoc format

--- Koin.java
+++ Koin.java
@@ -1,40 +1,74 @@
-class Koin {
-    private int nilai;
-    private String nama;
+/**
+ * Kelas Koin digunakan untuk merepresentasikan koin dengan nilai dan nama
+ * tertentu.
+ */
+public class Koin {
+    private int nilai; // Nilai koin
+    private String nama; // Nama koin
 
+    /**
+     * Konstruktor default untuk kelas Koin.
+     */
     public Koin() {
         // Konstruktor default
     }
 
+    /**
+     * Konstruktor untuk membuat objek Koin dengan nama dan nilai tertentu.
+     *
+     * @param nama  Nama koin.
+     * @param nilai Nilai koin.
+     */
     public Koin(String nama, int nilai) {
         this.nama = nama;
         this.nilai = nilai;
     }
 
+    /**
+     * Mendapatkan nilai koin.
+     *
+     * @return Nilai koin.
+     */
     public int getNilai() {
         return this.nilai;
     }
 
+    /**
+     * Mengatur nilai koin.
+     *
+     * @param nilai Nilai koin yang akan diatur.
+     */
     public void setNilai(int nilai) {
         this.nilai = nilai;
     }
 
-    public String getNama(){
+    /**
+     * Mendapatkan nama koin.
+     *
+     * @return Nama koin.
+     */
+    public String getNama() {
         return nama;
     }
 
+    /**
+     * Mendapatkan jenis koin berdasarkan nilai tertentu.
+     *
+     * @param nilai Nilai koin yang akan digunakan untuk menentukan jenis.
+     * @return Nama jenis koin (Bronze, Silver, Gold, Platinum) atau null jika nilai
+     *         tidak valid.
+     */
     public String getJenisKoinByNilai(int nilai) {
         if (nilai >= 1 && nilai <= 40) {
-            return this.nama = "Bronze";
+            return "Bronze";
         } else if (nilai >= 41 && nilai <= 75) {
-            return this.nama = "Silver";
+            return "Silver";
         } else if (nilai >= 76 && nilai <= 120) {
-            return this.nama = "Gold";
+            return "Gold";
         } else if (nilai >= 121 && nilai <= 170) {
-            return this.nama = "Platinum";
+            return "Platinum";
         } else {
             return null;
         }
     }
-
 }
