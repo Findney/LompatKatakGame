2f3345b638e37d90c63b964672fe060ae654c60e
archfi <mughni@mhs.usk.ac.id>
2023-10-29 11:07:49 +0700

    Add comment and javadoc format

--- MainLompatKatak.java
+++ MainLompatKatak.java
@@ -1,7 +1,30 @@
-public class MainLompatKatak {
+/**
+ * Ini adalah program permainan Katak Petualang. Katak akan bergerak di papan permainan
+ * dan mendapatkan koin atau bertemu monster. Pemain dapat memilih level permainan, 
+ * menggerakkan katak, dan melihat hasil akhir.
+ * <p>  
+ * Pada versi kali ini terdapat beberapa perubahan yaitu penggunaan konsep OOP, penambahan
+ * level Impossible, penambahan jenis-jenis koin dan monster yang sesuai berdasarkan level 
+ * yang dipilih oleh pemain. 
+ *
+ * @author Agil Mughni
+ * @version 1.2
+ * @since 2023-10-08
+ * @update 2023-10-29
+ */
 
+public class MainLompatKatak {
+    /**
+     * Metode utama (main) dari aplikasi ini. Ini merupakan titik masuk untuk menjalankan 
+     * permainan Lompat Katak.
+     *
+     * @param args Argumen baris perintah (tidak digunakan dalam permainan ini).
+     */
     public static void main(String[] args) {
+         // Membuat objek permainan LompatKatak.
         LompatKatak game = new LompatKatak();
+
+        // Memulai permainan.
         game.mainkan();
     }
 }
