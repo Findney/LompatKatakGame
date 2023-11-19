5b13cd0dfb32eace28e82b2a981c94b9c8e52573
archfi <mughni@mhs.usk.ac.id>
2023-11-19 13:24:39 +0700

    add exception

--- LompatKatak.java
+++ LompatKatak.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -29,26 +30,43 @@ public class LompatKatak {
             System.out.println("+-------------------------------------+");
             System.out.print("Masukkan nama pemain: ");
             String namaPemain = input.nextLine();
-            
+
             // Membuat objek dari kelas Pemain
             Pemain pemain = new Pemain(namaPemain);
 
             // Memilih level permainan
             System.out.println("+-------------------------------------+");
             System.out.println("| Pilih level permainan:              |");
-            System.out.println("| 1. "+pemain.getNamaLevel(1)+"                             |");
-            System.out.println("| 2. "+pemain.getNamaLevel(2)+"                     |");
-            System.out.println("| 3. "+pemain.getNamaLevel(3)+"                             |");
-            System.out.println("| 4. "+pemain.getNamaLevel(4)+"                       |");
+            System.out.println("| 1. " + pemain.getNamaLevel(1) + "                             |");
+            System.out.println("| 2. " + pemain.getNamaLevel(2) + "                     |");
+            System.out.println("| 3. " + pemain.getNamaLevel(3) + "                             |");
+            System.out.println("| 4. " + pemain.getNamaLevel(4) + "                       |");
             System.out.println("+-------------------------------------+");
-            System.out.print("Masukkan pilihan: ");
-            int pilihanLevel = input.nextInt();
 
-            // Validasi pemilihan level
-            while (pilihanLevel < 1 || pilihanLevel > 4) {
-                System.out.println("Pilihan LEVEL TIDAK VALID. Harap pilih level 1, 2, 3 atau 4.");
-                System.out.print("Masukkan pilihan level: ");
-                pilihanLevel = input.nextInt();
+            int pilihanLevel = 0;
+            while (true) {
+                try {
+                    System.out.print("Masukkan pilihan: ");
+                    pilihanLevel = input.nextInt();
+                    if (pilihanLevel >= 1 && pilihanLevel <= 4) {
+                        break;
+                    } else {
+                        System.out.println(
+                                "\n********************************************************************************");
+                        System.out.println("Pilihan LEVEL TIDAK VALID. Harap pilih level 1, 2, 3 atau 4.");
+                        System.out.println(
+                                "********************************************************************************");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println(
+                            "\n********************************************************************************");
+                    System.out.println("Input harus berupa ANGKA. Pilihan terdiri dari 1, 2, 3, atau 4.");
+                    System.out.println(
+                            "********************************************************************************");
+
+                    // Mengambil karakter '\n' saat ENTER dari sisa buffer sebelumnya
+                    input.nextLine();
+                }
             }
 
             int jumlahKoin = 0;
@@ -75,12 +93,12 @@ public class LompatKatak {
                 default:
                     break;
             }
-            
+
             // Membuat objek dari kelas Monster
             Monster monster = new Monster();
             // Membuat objek dari kelas Koin
             Koin koin = new Koin();
-            // Membuat objek dari kelas Katak       
+            // Membuat objek dari kelas Katak
             Katak katak = new Katak();
             // Membuat objek dari kelas KotakPermainan
             KotakPermainan permainan = new KotakPermainan(300, jumlahKoin, jumlahMonster, pilihanLevel, monster, koin);
@@ -107,18 +125,31 @@ public class LompatKatak {
                     System.out.println(" 2. Mundur");
                     System.out.println("----------------------------------------------");
 
-                    System.out.print("Masukkan pilihan arah pergerakan: ");
-                    katak.setPilihanArah(input.nextInt());
+                    while (true) {
+                        try {
+                            System.out.print("Masukkan pilihan arah pergerakan: ");
+                            katak.setPilihanArah(input.nextInt());
 
-                    // Validasi pilihan arah
-                    while (katak.getPilihanArah() < 1 || katak.getPilihanArah() > 2) {
-                        System.out.println("\n********************************************************************************");
-                        System.out.println("Pilihan arah TIDAK VALID. Masukkan pilihan yang sesuai (1 atau 2).");
-                        System.out.println("********************************************************************************");  
-                        System.out.print("Masukkan pilihan arah pergerakan: ");
-                        katak.setPilihanArah(input.nextInt());
+                            // Validasi pilihan arah
+                            if (katak.getPilihanArah() == 1 || katak.getPilihanArah() == 2) {
+                                break;
+                            } else {
+                                System.out.println(
+                                        "\n********************************************************************************");
+                                System.out
+                                        .println("Pilihan ARAH TIDAK VALID. Masukkan pilihan yang sesuai (1 atau 2).");
+                                System.out.println(
+                                        "********************************************************************************");
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println(
+                                    "\n********************************************************************************");
+                            System.out.println("Input harus berupa ANGKA. Pilihan terdiri dari 1 atau 2.");
+                            System.out.println(
+                                    "********************************************************************************");
+                            input.nextLine();
+                        }
                     }
-
                     if (katak.getPilihanArah() == 2) {
                         katak.setArah(false);
                     } else {
@@ -126,29 +157,41 @@ public class LompatKatak {
                     }
                 }
 
-                // Meminta jumlah kotak yang akan dilewati
-                System.out.print("Masukkan jumlah kotak yang ingin dilewati: ");
-                permainan.setJumlahLompatan(input.nextInt());
+                while (true) {
+                    try {
+                        // Meminta jumlah kotak yang akan dilewati
+                        System.out.print("Masukkan jumlah kotak yang ingin dilewati: ");
+                        permainan.setJumlahLompatan(input.nextInt());
+
+                        // Validasi jumlah lompatan
+                        if (permainan.getJumlahLompatan() == 1 || permainan.getJumlahLompatan() == 2) {
+                            // Validasi jumlah lompatan agar tidak melewati kotak 0 saat mundur
+                            if (!katak.getArah() && permainan.getJumlahLompatan() > katak.getPosisi()) {
+                                System.out
+                                        .println("\n****************************************************************");
+                                System.out.println("Jumlah lompatan untuk mundur TIDAK BOLEH melewati kotak 0.");
+                                System.out.println("****************************************************************");
+                            } else {
+                                break;
+                            }
+                        } else {
+                            System.out.println(
+                                    "\n********************************************************************************");
+                            System.out.println(
+                                    "Pilihan LANGKAH TIDAK VALID. Masukkan pilihan yang sesuai (1 atau 2).");
+                            System.out.println(
+                                    "********************************************************************************");
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println(
+                                "\n********************************************************************************");
+                        System.out.println("Input harus berupa ANGKA. Pilihan terdiri dari 1 atau 2.");
+                        System.out.println(
+                                "********************************************************************************");
+                        input.nextLine();
+                    }
+                }
                 System.out.println("+--------------------------------------------+");
-
-
-                // Validasi jumlah lompatan
-                while (permainan.getJumlahLompatan() < 1 || permainan.getJumlahLompatan() > 2) {
-                    System.out.println("\n********************************************************************************");                    
-                    System.out.println("Pilihan langkah TIDAK VALID. Masukkan jumlah kotak yang sesuai (1 atau 2).");
-                    System.out.println("********************************************************************************");                                        
-                    System.out.print("Masukkan jumlah kotak yang ingin dilewati: ");
-                    permainan.setJumlahLompatan(input.nextInt());
-                }
-
-                // Validasi jumlah lompatan agar tidak melewati kotak 0 saat mundur
-                while (!katak.getArah() && permainan.getJumlahLompatan() > katak.getPosisi()) {
-                    System.out.println("\n****************************************************************");
-                    System.out.println("Jumlah lompatan untuk mundur TIDAK BOLEH melewati kotak 0.");
-                    System.out.println("****************************************************************");                    
-                    System.out.print("Masukkan jumlah kotak yang ingin dilewati: ");
-                    permainan.setJumlahLompatan(input.nextInt());
-                }
 
                 // Melakukan lompatan sesuai jumlah lompatan yang dimasukkan
                 if (permainan.getJumlahLompatan() == 1) {
@@ -156,23 +199,27 @@ public class LompatKatak {
                 } else if (permainan.getJumlahLompatan() == 2) {
                     katak.loncatJauh();
                 }
-
                 // Mengambil skor dari kotak yang dilewati
                 int skor = permainan.contain(katak.getPosisi());
 
                 // Menentukan apakah kotak berisi koin atau monster
                 if (skor > 0) {
                     katak.setSkor(katak.getSkor() + skor);
-                    System.out.println("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-                    System.out.println("Selamat! " + pemain.getNama() + " menemukan koin " + koin.getNama() + " dan mendapatkan " + skor + " poin.");
-                    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                    System.out.println(
+                            "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                    System.out.println("Selamat! " + pemain.getNama() + " menemukan koin " + koin.getNama()
+                            + " dan mendapatkan " + skor + " poin.");
+                    System.out.println(
+                            "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 
                 } else if (skor < 0) {
                     katak.setSkor(katak.getSkor() + skor);
-                    System.out.println("\n------------------------------------------------------------------------------");
+                    System.out.println(
+                            "\n------------------------------------------------------------------------------");
                     System.out.println("Sayang sekali! " + pemain.getNama() + " menemukan monster " + monster.getNama()
                             + " dan kehilangan " + (-skor) + " poin.");
-                    System.out.println("------------------------------------------------------------------------------\n");
+                    System.out.println(
+                            "------------------------------------------------------------------------------\n");
 
                 }
 
@@ -193,14 +240,14 @@ public class LompatKatak {
             }
 
             // Mengambil nilai dari karakter (Katak) dan mengatur nilai pemain
-            pemain.setNilai(katak.getSkor()); 
+            pemain.setNilai(katak.getSkor());
 
             // Tampilkan skor akhir dan komentar
             System.out.println("+-------------------------------------+");
             System.out.println("|          PERMAINAN BERAKHIR         |");
             System.out.println("+-------------------------------------+");
             System.out.println(" Pemain: " + pemain.getNama());
-            System.out.println(" Nilai total: "+ pemain.getNilai());
+            System.out.println(" Nilai total: " + pemain.getNilai());
             System.out.println(" Level permainan: " + pemain.getNamaLevel(pilihanLevel));
             System.out.println(" Komentar: " + komentar);
             System.out.println("+-------------------------------------+");
@@ -213,12 +260,12 @@ public class LompatKatak {
                 System.out.print("Apakah Anda ingin melanjutkan bermain? [y/n]: ");
                 jawaban = input.next();
             }
-                        
+
             if (!jawaban.equalsIgnoreCase("y")) {
                 play = false;
                 System.out.println("Terima Kasih Telah Bermain.");
             }
-            
+
             // Mengambil karakter '\n' saat ENTER dari sisa buffer sebelumnya
             input.nextLine();
         }
