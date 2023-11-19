e42d15c866296b3f4bfb2e1eed6e167d56873dfa
archfi <mughni@mhs.usk.ac.id>
2023-11-19 13:22:11 +0700

    make it inheritance from Makhluk class

--- Monster.java
+++ Monster.java
@@ -1,61 +1,42 @@
 /**
- * Kelas Monster digunakan untuk merepresentasikan makhluk-makhluk dalam permainan.
+ * Kelas Monster merupakan turunan dari kelas Makhluk, merepresentasikan monster dalam suatu permainan.
  */
-class Monster {
-    private int nilai;
-    private String nama;
+public class Monster extends Makhluk {
 
     /**
-     * Konstruktor default untuk kelas Monster.
+     * Konstruktor default untuk objek Monster.
      */
     public Monster() {
-        // Konstruktor default
+        // Kosong karena tidak ada inisialisasi khusus yang diperlukan.
     }
 
     /**
-     * Konstruktor parameter untuk kelas Monster.
+     * Konstruktor untuk membuat objek Monster dengan nama dan nilai tertentu.
      *
-     * @param nama Nama monster.
-     * @param nilai Nilai atau tingkat monster.
+     * @param nama  Nama monster.
+     * @param nilai Nilai monster.
      */
     public Monster(String nama, int nilai) {
-        this.nama = nama;
-        this.nilai = nilai;
+        super(nama, nilai);
     }
 
     /**
-     * Mendapatkan nilai monster.
+     * Mengembalikan jenis nama monster berdasarkan nilai tertentu.
      *
-     * @return Nilai monster.
+     * @param nilai Nilai monster.
+     * @return Nama jenis monster (Goblin, Witch, Dragon, Godzilla) berdasarkan nilai.
+     *         Jika nilai tidak sesuai dengan kriteria, mengembalikan null.
      */
-    public int getNilai() {
-        return this.nilai;
-    }
-
-    /**
-     * Mendapatkan nama monster.
-     *
-     * @return Nama monster.
-     */
-    public String getNama() {
-        return this.nama;
-    }
-
-    /**
-     * Mendapatkan jenis monster berdasarkan nilai tertentu.
-     *
-     * @param nilai Nilai yang akan digunakan untuk menentukan jenis monster.
-     * @return Nama jenis monster berdasarkan nilai yang diberikan.
-     */
-    public String getJenisMonsterByNilai(int nilai) {
+    @Override
+    public String getJenisNamaByNilai(int nilai) {
         if (nilai <= -1 && nilai >= -40) {
-            return this.nama="Goblin";
+            return this.nama = "Goblin";
         } else if (nilai <= -41 && nilai >= -75) {
-            return this.nama="Witch";
+            return this.nama = "Witch";
         } else if (nilai <= -76 && nilai >= -120) {
-            return this.nama="Dragon";
+            return this.nama = "Dragon";
         } else if (nilai <= -121 && nilai >= -170) {
-            return this.nama="Godzilla";
+            return this.nama = "Godzilla";
         } else {
             return null;
         }
