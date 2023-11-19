658893a272e379f640fbc99ac190cdf2d50de7d2
archfi <mughni@mhs.usk.ac.id>
2023-11-19 14:30:10 +0700

    add exception

--- MainLompatKatak.java
+++ MainLompatKatak.java
@@ -1,30 +1,44 @@
+
+// Link Video Penjelasan: 
+
 /**
- * Ini adalah program permainan Katak Petualang. Katak akan bergerak di papan permainan
- * dan mendapatkan koin atau bertemu monster. Pemain dapat memilih level permainan, 
+ * Ini adalah program permainan Katak Petualang. Katak akan bergerak di papan
+ * permainan
+ * dan mendapatkan koin atau bertemu monster. Pemain dapat memilih level
+ * permainan,
  * menggerakkan katak, dan melihat hasil akhir.
- * <p>  
- * Pada versi kali ini terdapat beberapa perubahan yaitu penggunaan konsep OOP, penambahan
- * level Impossible, penambahan jenis-jenis koin dan monster yang sesuai berdasarkan level 
- * yang dipilih oleh pemain. 
+ * <p>
+ * Pada versi kali ini terdapat beberapa perubahan yaitu penggunaan konsep kelas abstrak,
+ * inheritance, collection, dan penggunaan exception handling.
  *
  * @author Agil Mughni
- * @version 1.2
+ * @version 1.3
  * @since 2023-10-08
- * @update 2023-10-29
+ * @update 2023-11-18
  */
 
 public class MainLompatKatak {
     /**
-     * Metode utama (main) dari aplikasi ini. Ini merupakan titik masuk untuk menjalankan 
+     * Metode utama (main) dari aplikasi ini. Ini merupakan titik masuk untuk
+     * menjalankan
      * permainan Lompat Katak.
      *
      * @param args Argumen baris perintah (tidak digunakan dalam permainan ini).
      */
     public static void main(String[] args) {
-         // Membuat objek permainan LompatKatak.
-        LompatKatak game = new LompatKatak();
+        try {
+            // Membuat objek permainan LompatKatak.
+            LompatKatak game = new LompatKatak();
 
-        // Memulai permainan.
-        game.mainkan();
+            // Memulai permainan.
+            game.mainkan();
+        } catch (Exception e) {
+            // Menangani exception jika terjadi kesalahan selama eksekusi permainan
+            System.out.println("Terjadi error: " + e.getMessage());
+
+            // Mencetak informasi kesalahan dan jejak eksekusi (stack trace)
+            e.printStackTrace();
+        } 
+
     }
 }
